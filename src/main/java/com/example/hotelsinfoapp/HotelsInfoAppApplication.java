364f2fb79cb1859baccd7e384939e3626161e23d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-06-04 00:00:48 +0300

    Create base project with migrations

---
+++ src/main/java/com/example/hotelsinfoapp/HotelsInfoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelsinfoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsInfoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsInfoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotelsinfoapp/HotelsInfoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelsinfoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsInfoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
