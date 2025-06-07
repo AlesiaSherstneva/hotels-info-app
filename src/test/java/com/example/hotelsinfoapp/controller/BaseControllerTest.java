9ecd909f7de6af05d5403498909d37ae6aa593d7
Alesia Sherstneva <alesiasherstneva@gmail.com>
2025-06-07 09:48:05 +0300

    Write integration tests

---
+++ src/test/java/com/example/hotelsinfoapp/controller/BaseControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.hotelsinfoapp.controller;
+
+import com.example.hotelsinfoapp.repository.AmenityRepository;
+import com.example.hotelsinfoapp.repository.HotelRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Random;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.Random.class)
+public class BaseControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected HotelRepository hotelRepository;
+
+    @Autowired
+    protected AmenityRepository amenityRepository;
+
+    protected static final Random RANDOM = new Random();
+}
